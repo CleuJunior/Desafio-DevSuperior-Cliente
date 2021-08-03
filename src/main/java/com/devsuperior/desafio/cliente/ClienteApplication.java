ca85f29a1a98255a7e20e70de1c302ca33367f90
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-08-03 20:36:36 -0300

    first commit

---
+++ src/main/java/com/devsuperior/desafio/cliente/ClienteApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafio/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
