03f06bce259d30a8c7e9a984377cb133c75a6a43
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-08-03 21:02:46 -0300

    Client Entity

---
+++ src/main/java/com/devsuperior/desafio/cliente/entities/Client.java
@@ -0,0 +1,68 @@
+package com.devsuperior.desafio.cliente.entities;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_client")
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String cpf;
+    private Double income;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant birthDate;
+    private Integer children;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getCpf() { return cpf; }
+
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public Double getIncome() { return income; }
+
+    public void setIncome(Double income) { this.income = income; }
+
+    public Instant getBirthDate() { return birthDate; }
+
+    public void setBirthDate(Instant birthDate) { this.birthDate = birthDate; }
+
+    public Integer getChildren() { return children; }
+
+    public void setChildren(Integer children) { this.children = children; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Client)) return false;
+        Client client = (Client) o;
+        return getId().equals(client.getId());
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(getId()); }
+
+}
