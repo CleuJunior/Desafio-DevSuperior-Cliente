03bd49af37eedb3243eba1b70d66ef11b472fb91
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-08-04 00:34:18 -0300

    Seed SQL

--- src/main/resources/date.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Caidir Hodarod', '62979592005', 3899.00, TIMESTAMP WITH TIME ZONE '1988-07-13T20:50:07.12345Z', 3);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Oslelas Beldayoric', '13787189092', 2310.00, TIMESTAMP WITH TIME ZONE '1999-10-19T20:50:07.12345Z', 1);
